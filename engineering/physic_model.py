b3a2e030c7fd3d40af7030d11efe763d923ba448
cimadure <ronan.cimadure@gmail.com>
2018-08-02 16:56:18 -0600

    remove tested lines

--- engineering/physic_model.py
+++ engineering/physic_model.py
@@ -1,29 +1,10 @@
-# from OMPython import OMCSessionZMQ
-#
-# omc = OMCSessionZMQ()
-# print(omc.sendExpression("getVersion()"))
-#
-# omc.sendExpression("loadModel(Modelica)")
-# omc.sendExpression("loadFile(getInstallationDirectoryPath() + \"/share/doc/omc/testmodels/BouncingBall.mo\")")
-# omc.sendExpression("instantiateModel(BouncingBall)")
-# print(omc.sendExpression("getClassNames()"))
-#
-# cmds = [
-#     "loadModel(Modelica)",
-#     "model test end test;",
-#     'loadFile(getInstallationDirectoryPath() + "/share/doc/omc/testmodels/BouncingBall.mo")',
-#     "simulate(BouncingBall)",
-#     "plot(h)"
-# ]
-# # for cmd in cmds:
-# #     answer = omc.sendExpression(cmd)
-# #     print("\n{}:\n{}".format(cmd, answer))
-
-
 from OMPython import ModelicaSystem
+import matplotlib.pyplot as plt
+
 file = "/usr/share/doc/omc/testmodels/BouncingBall.mo"
 #file = omc.sendExpression("getInstallationDirectoryPath() + \"/share/doc/omc/testmodels/BouncingBall.mo\"")
 print(file)
+
 package = "BouncingBall"
 mod = ModelicaSystem(file, package)
 mod.setSimulationOptions(stopTime=3.0, tolerance=1e-08)
@@ -33,7 +14,6 @@ print(mod.getQuantities())
 
 time, height = mod.getSolutions("time", "h")
 
-import matplotlib.pyplot as plt
 plt.plot(time, height)
 plt.xlabel('time (s)')
 plt.ylabel('height of falling (m)')
